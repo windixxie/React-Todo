3d4abd6bcc7068803a3e59e1b27b3ce319748206
Winnie Song <winniesong0121@gmail.com>
2019-05-29 17:19:35 -0400

    MVP and all stretch finished

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const TodoForm = props => (
+    <form>
+        <input type='text' 
+            value={props.input}
+            placeholder='Enter a ToDo'
+            onChange={props.handleChange}
+            onKeyDown={(e) => e.keyCode === 13 ? props.addTodo(e) : null}
+        />
+        <input type="text" 
+            value={props.searchInput}
+            placeholder="Search Here"
+            onChange={props.onSearchChange}
+        />
+        <button type="button" onClick={props.addTodo} className="add-btn">Add Todo</button>
+        <button type='button' onClick={props.clearComplete}>Clear Completed</button>
+        <button type="button" onClick={props.clearAll}>Clear All</button>
+    </form>
+);
+
+export default TodoForm; 
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,13 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+import Todo from './Todo.js';
+
+const TodoList = props => (
+    <ul>
+        {props.list.map(item => <Todo key={item.id} markComplete={props.markComplete} item={item} />)}
+    </ul>
+);
+
+export default TodoList; 
